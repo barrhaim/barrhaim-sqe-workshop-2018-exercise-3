fe30685c4d29314fdf97fcae95e7fb6ed375f96c
barrhaim <barrhaim@gmail.com>
2018-12-29 11:29:20 +0200

    init

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,9 +1,7 @@
 import * as esprima from 'esprima';
-const myParseCode = codeToParse => {
-    return esprima.parseScript(codeToParse, { loc: true });
-};
-const parseCode = codeToParse => {
+
+const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse);
 };
 
-export { parseCode, myParseCode };
+export {parseCode};
