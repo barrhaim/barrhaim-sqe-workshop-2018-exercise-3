9babf4154a8a1ac39fc624a033be6ebc9fc2408f
barrhaim <barrhaim@gmail.com>
2019-01-02 10:16:10 +0200

    rdy

--- src/js/graph-gen.js
+++ src/js/graph-gen.js
@@ -1,22 +1,23 @@
 import * as esprima from 'esprima';
+
 const esgraph = require('esgraph');
 import * as escodegen from 'escodegen';
 
-function makegraph(source,inputs){
-    let ast = esprima.parse(source, { range: true });
+function makegraph(source, inputs) {
+    let ast = esprima.parse(source, {range: true});
     let params = extract_params(ast);
-    let env = make_vars_string(inputs,params);
+    let env = make_vars_string(inputs, params);
     let cfg = esgraph(handle_ast(ast));
     handle_cfg(cfg);
-    color_nodes(cfg,env);
+    color_nodes(cfg, env);
     let myformat = my_dot_format(cfg);
-    let dot = esgraph.dot(cfg, { counter: 0, source: source });
+    let dot = esgraph.dot(cfg, {counter: 0, source: source});
     dot = `digraph cfg { forcelabels=true\n ${myformat} }`;
     return dot;
 }
 
 
-function my_dot_format(cfg){
+function my_dot_format(cfg) {
     let nodes = make_nodes(cfg);
     let edges = make_edges(cfg);
     return nodes.concat(edges).join('');
@@ -36,10 +37,10 @@ function specify_node(current, i, s) {
     s.push(temp + ']\n');
 }
 
-function make_nodes(cfg){
-    let s=[];
+function make_nodes(cfg) {
+    let s = [];
     let nodes = cfg[2];
-    for(let i = 0 ; i < nodes.length;i++){
+    for (let i = 0; i < nodes.length; i++) {
         let current = nodes[i];
         specify_node(current, i, s);
     }
@@ -64,10 +65,10 @@ function gen_edge(x, nodes, edges) {
     }
 }
 
-function make_edges(cfg){
+function make_edges(cfg) {
     let edges = [];
     let nodes = cfg[2];
-    nodes.forEach(x=>{
+    nodes.forEach(x => {
         gen_edge(x, nodes, edges);
 
     });
@@ -75,11 +76,10 @@ function make_edges(cfg){
 }
 
 
-
 ///////////////////////////
-function color_nodes(cfg,env){
+function color_nodes(cfg, env) {
     let gnodes = cfg[2];
-    rec_coloring(gnodes[0],env);
+    rec_coloring(gnodes[0], env);
 }
 
 //FINITE VERSION
@@ -98,51 +98,53 @@ function color_nodes(cfg,env){
 
 //ENDLESS IN SOME CASES
 function rec_coloring(node, env) {
-    if (node){
-        if(node.true && node.false){
-            handle_condition(node,env);
+    if (node) {
+        if (node.true && node.false) {
+            handle_condition(node, env);
         }
-        else{
-            node.color='green';
-            rec_coloring(node.normal,env+node.label);
+        else {
+            node.color = 'green';
+            rec_coloring(node.normal, env + node.label);
         }
     }
 
 }
-function handle_condition(node,env){
-    node.color='green';
 
-    if(eval(env+node.label)){
-        rec_coloring(node.true,env);
+function handle_condition(node, env) {
+    node.color = 'green';
+
+    if (eval(env + node.label)) {
+        rec_coloring(node.true, env);
     }
-    else{
-        rec_coloring(node.false,env);
+    else {
+        rec_coloring(node.false, env);
     }
 }
-function make_vars_string(inputs,params){
+
+function make_vars_string(inputs, params) {
     let data = eval(inputs);
     let vs = [];
-    for(let i =0 ; i<data.length ; i++){
-        vs.push(`let ${params[i]} = ${data[i]};`);
+    for (let i = 0; i < data.length; i++) {
+        if (Array.isArray(data[i])) {
+            vs.push(`let ${params[i]} = [${data[i]}];`);
+        }
+        else {
+            vs.push(`let ${params[i]} = ${data[i]};`);
+        }
+
     }
     return vs.join('');
 }
 
 function extract_params(ast) {
-    let param_names =[];
-    ast.body[0].params.forEach(p=>param_names.push(p.name));
+    let param_names = [];
+    ast.body[0].params.forEach(p => param_names.push(p.name));
     return param_names;
 }
 
 
-
-
-
-
-
-
 //////////////////////////////////////////////////////////
-function handle_ast(ast){
+function handle_ast(ast) {
     return ast.body[0].body;
 }
 
@@ -158,7 +160,7 @@ function del_exit_node(gnodes) {
 function lables_and_exceptions(gnodes) {
     gnodes.forEach(x => {
         // if (x.astNode !== undefined) {
-        x.label = escodegen.generate(x.astNode)+';';
+        x.label = escodegen.generate(x.astNode) + ';';
         delete x.exception;
         // }
     });
@@ -166,8 +168,8 @@ function lables_and_exceptions(gnodes) {
 
 function handle_cfg(cfg) {
     let gnodes = cfg[2];
-    gnodes.splice(0,1);
-    gnodes.splice(gnodes.length-1,1);
+    gnodes.splice(0, 1);
+    gnodes.splice(gnodes.length - 1, 1);
     lables_and_exceptions(gnodes);
     del_exit_node(gnodes);
     merge(gnodes);
@@ -196,4 +198,4 @@ function merge(cfg) {
 //////////////////////////////////////////////////////////
 
 
-export {makegraph};
+export {makegraph, make_vars_string};
--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import {parseCode} from '../src/js/code-analyzer';
-import {makegraph} from '../src/js/graph-gen';
+import {makegraph, make_vars_string} from '../src/js/graph-gen';
 
 describe('The javascript parser', () => {
     it('is parsing an empty function correctly', () => {
@@ -19,31 +19,102 @@ describe('The javascript parser', () => {
 });
 
 describe('dot format resulted from if elseif else', () => {
-    it('ex1 dot format', () => {
+    it('if else branching test', () => {
         const source = 'function foo(x, y, z){\n' + '    let a = x + 1;\n' + '    let b = a + y;\n' + '    let c = 0;\n' + '    \n' + '    if (b < z) {\n' + '        c = c + 5;\n' + '    } else if (b < z * 2) {\n' + '        c = c + x + 5;\n' + '    } else {\n' + '        c = c + z + 5;\n' + '    }\n' + '    \n' + '    return c;\n' + '}\n';
-        const input='[1,5,0]';
-        let dot = makegraph(source,input);
+        const input = '[1,5,0]';
+        let dot = makegraph(source, input);
         let expect = 'digraph cfg { forcelabels=true\n n0 [label="let a = x + 1;;\nlet b = a + y;;\nlet c = 0;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\nn1 [label="b < z;",xlabel="1", shape=diamond, style = filled, fillcolor=green ]\nn2 [label="c = c + 5;",xlabel="2", shape=rectangle]\nn3 [label="return c;;",xlabel="3", shape=rectangle, style = filled, fillcolor=green ]\nn4 [label="b < z * 2;",xlabel="4", shape=diamond, style = filled, fillcolor=green ]\nn5 [label="c = c + x + 5;",xlabel="5", shape=rectangle]\nn6 [label="c = c + z + 5;",xlabel="6", shape=rectangle, style = filled, fillcolor=green ]\nn0 -> n1 []\nn1 -> n2 [label="true"]\nn1 -> n4 [label="false"]\nn2 -> n3 []\nn4 -> n5 [label="true"]\nn4 -> n6 [label="false"]\nn5 -> n3 []\nn6 -> n3 []\n }';
-        assert.equal(expect,dot);
+        assert.equal(expect, dot);
     });
 });
 
 describe('dot format resulted from while example', () => {
-    it('ex1 dot format', () => {
-        const source ='function foo(x, y, z){\n' + '   let a = x + 1;\n' + '   let b = a + y;\n' + '   let c = 0;\n' + '   \n' + '   while (a < z) {\n' + '       c = a + b;\n' + '       z = c * 2;\n' + '       a++;\n' + '   }\n' + '   \n' + '   return z;\n' + '}\n';
-        const input='[80,5,0]';
-        let dot = makegraph(source,input);
-        let expect ='digraph cfg { forcelabels=true\n n0 [label="let a = x + 1;;\nlet b = a + y;;\nlet c = 0;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\nn1 [label="a < z;",xlabel="1", shape=diamond, style = filled, fillcolor=green ]\nn2 [label="c = a + b;\nz = c * 2;\na++;",xlabel="2", shape=rectangle]\nn3 [label="return z;;",xlabel="3", shape=rectangle, style = filled, fillcolor=green ]\nn0 -> n1 []\nn1 -> n2 [label="true"]\nn1 -> n3 [label="false"]\nn2 -> n1 []\n }';
-        assert.equal(expect,dot);
+    it('while branching test', () => {
+        const source = 'function foo(x, y, z){\n' + '   let a = x + 1;\n' + '   let b = a + y;\n' + '   let c = 0;\n' + '   \n' + '   while (a < z) {\n' + '       c = a + b;\n' + '       z = c * 2;\n' + '       a++;\n' + '   }\n' + '   \n' + '   return z;\n' + '}\n';
+        const input = '[80,5,0]';
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="let a = x + 1;;\nlet b = a + y;;\nlet c = 0;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\nn1 [label="a < z;",xlabel="1", shape=diamond, style = filled, fillcolor=green ]\nn2 [label="c = a + b;\nz = c * 2;\na++;",xlabel="2", shape=rectangle]\nn3 [label="return z;;",xlabel="3", shape=rectangle, style = filled, fillcolor=green ]\nn0 -> n1 []\nn1 -> n2 [label="true"]\nn1 -> n3 [label="false"]\nn2 -> n1 []\n }';
+        assert.equal(expect, dot);
     });
 });
 
 describe('all is green peace', () => {
-    it('ex1 dot format', () => {
-        const source='function foo(x, y, z){\n' + '   let a = x;\n' + '   let b = a;\n' + '   let c = 0;\n' + '   \n' + '   if(a > z) {\n' + '       c = a + b;\n' + '       z = c * 2;\n' + '       a++;\n' + '   }\n' + '   \n' + '   return z;\n' + '}\n';
-        const input='[80,5,0]';
-        let dot = makegraph(source,input);
+    it('fully green test', () => {
+        const source = 'function foo(x, y, z){\n' + '   let a = x;\n' + '   let b = a;\n' + '   let c = 0;\n' + '   \n' + '   if(a > z) {\n' + '       c = a + b;\n' + '       z = c * 2;\n' + '       a++;\n' + '   }\n' + '   \n' + '   return z;\n' + '}\n';
+        const input = '[80,5,0]';
+        let dot = makegraph(source, input);
         let expect = 'digraph cfg { forcelabels=true\n n0 [label="let a = x;;\nlet b = a;;\nlet c = 0;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\nn1 [label="a > z;",xlabel="1", shape=diamond, style = filled, fillcolor=green ]\nn2 [label="c = a + b;\nz = c * 2;\na++;",xlabel="2", shape=rectangle, style = filled, fillcolor=green ]\nn3 [label="return z;;",xlabel="3", shape=rectangle, style = filled, fillcolor=green ]\nn0 -> n1 []\nn1 -> n2 [label="true"]\nn1 -> n3 [label="false"]\nn2 -> n3 []\n }';
-        assert.equal(expect,dot);
+        assert.equal(expect, dot);
+    });
+});
+
+describe('translate user input to env', () => {
+    it('simple env', () => {
+        const input = '[1,2,3]';
+        const params = ['x', 'y', 'z'];
+        let makeVarsString = make_vars_string(input, params);
+        const expect = 'let x = 1;let y = 2;let z = 3;';
+        assert.equal(expect, makeVarsString);
+    });
+});
+
+describe('simple branching false taken', () => {
+    it('false path taken', () => {
+        const source = 'function foo(x){\n' + 'if(x>3){\n' + 'return 4;\n' + '}\n' + 'else {\n' + 'return 5;\n' + '}\n' + '\n' + '}\n';
+        const input = '[1]';
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="x > 3;",xlabel="0", shape=diamond, style = filled, fillcolor=green ]\nn1 [label="return 4;;",xlabel="1", shape=rectangle]\nn2 [label="return 5;;",xlabel="2", shape=rectangle, style = filled, fillcolor=green ]\nn0 -> n1 [label="true"]\nn0 -> n2 [label="false"]\n }';
+        assert.equal(expect, dot);
+    });
+});
+
+describe('simple branching true taken', () => {
+    it('true path taken', () => {
+        const source = 'function foo(x){\n' + 'if(x>3){\n' + 'return 4;\n' + '}\n' + 'else {\n' + 'return 5;\n' + '}\n' + '\n' + '}\n';
+        const input = '[10]';
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="x > 3;",xlabel="0", shape=diamond, style = filled, fillcolor=green ]\nn1 [label="return 4;;",xlabel="1", shape=rectangle, style = filled, fillcolor=green ]\nn2 [label="return 5;;",xlabel="2", shape=rectangle]\nn0 -> n1 [label="true"]\nn0 -> n2 [label="false"]\n }';
+        assert.equal(expect, dot);
+    });
+});
+
+describe('just return in body', () => {
+    it('return only and string input', () => {
+        const source = 'function foo(x){\n' + 'return x;\n' + '}\n';
+        const input = ['bla'];
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="return x;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\n }';
+        assert.equal(expect, dot);
+    });
+});
+
+describe('parse boolean', () => {
+    it('its boolean age', () => {
+        const source = 'function foo(x){\n' + 'return x;\n' + '}\n';
+        const input = ['true'];
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="return x;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\n }';
+        assert.equal(expect, dot);
+    });
+});
+
+
+describe('array parsing', () => {
+    it('array parsing', () => {
+        const source = 'function foo(x){\n' + 'return x;\n' + '}\n';
+        const input = [[1, 2]];
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="return x;;",xlabel="0", shape=rectangle, style = filled, fillcolor=green ]\n }';
+        assert.equal(expect, dot);
+    });
+});
+
+describe('complex parsing', () => {
+    it('arrays and whiles and ifs', () => {
+        const source = 'function foo(x ,y){\n' + '\n' + 'if (x[0] < 3) {\n' + '   c = c + 5;\n' + '} \n' + '\n' + 'while(y<10) {\n' + ' y++;\n' + '}\n' + '    \n' + 'return c;\n' + '}\n';
+        const input = [[10], 1];
+        let dot = makegraph(source, input);
+        let expect = 'digraph cfg { forcelabels=true\n n0 [label="x[0] < 3;",xlabel="0", shape=diamond, style = filled, fillcolor=green ]\nn1 [label="c = c + 5;",xlabel="1", shape=rectangle]\nn2 [label="y < 10;",xlabel="2", shape=diamond, style = filled, fillcolor=green ]\nn3 [label="y++;",xlabel="3", shape=rectangle, style = filled, fillcolor=green ]\nn4 [label="return c;;",xlabel="4", shape=rectangle, style = filled, fillcolor=green ]\nn0 -> n1 [label="true"]\nn0 -> n2 [label="false"]\nn1 -> n2 []\nn2 -> n3 [label="true"]\nn2 -> n4 [label="false"]\nn3 -> n2 []\n }';
+        assert.equal(expect, dot);
     });
 });
